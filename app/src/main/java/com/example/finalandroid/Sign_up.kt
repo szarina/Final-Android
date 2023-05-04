e53dbc37bb8f7ca1f81a580987e11d6a907e8e79
Aruzhan <aruzhansarsem57@gmail.com>
2023-05-04 22:04:35 +0600

    first

--- app/src/main/java/com/example/finalandroid/Sign_up.kt
+++ app/src/main/java/com/example/finalandroid/Sign_up.kt
@@ -7,5 +7,7 @@ class Sign_up : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_up)
+
+
     }
 }
