f16d8123be5fef290e323a9f4a01031beb11f9f1
szarina <zarina.saden@gmail.com>
2023-05-08 14:18:10 +0600

    new func

--- app/src/main/java/com/example/finalandroid/api/API_service.kt
+++ app/src/main/java/com/example/finalandroid/api/API_service.kt
@@ -1,10 +1,22 @@
 package com.example.finalandroid.api
 
 import com.example.finalandroid.data_classes.Film
+import com.example.finalandroid.data_classes.User
 import retrofit2.Call
 import retrofit2.http.GET
+import retrofit2.http.Path
 
 interface API_service {
-    @GET("films") //?country=us&category=business&apiKey=8ad925c3ce3a4309b73812315bbc7e97
+    @GET("films")
     fun getFilms(): Call<ArrayList<Film>>
+
+    @GET("films/{film_id}")
+    fun getFilm(@Path("film_id") film_id: Int): Call<Film>
+
+    @GET("users")
+    fun getUsers(): Call <ArrayList <User> >
+
+
+
+
 }
