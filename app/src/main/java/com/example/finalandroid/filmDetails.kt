c4c5bbee2288ca88ac5916180fe8e8d11096568c
Togzhan Kali <t_kali@kbtu.kz>
2023-05-11 16:51:51 +0600

    fcfc

--- app/src/main/java/com/example/finalandroid/filmDetails.kt
+++ app/src/main/java/com/example/finalandroid/filmDetails.kt
@@ -4,10 +4,13 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
+import com.bumptech.glide.Glide
+import com.example.finalandroid.comment.Comment
 import com.example.finalandroid.databinding.ActivityFilmDetailsBinding
 
 class filmDetails : AppCompatActivity() {
     lateinit var binding: ActivityFilmDetailsBinding
+    lateinit var bundle: Bundle
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -24,5 +27,26 @@ class filmDetails : AppCompatActivity() {
             Toast.makeText(this@filmDetails,
                 "Rating is: "+msg, Toast.LENGTH_SHORT).show()
         }
+
+        binding.saveImgBtn.setOnClickListener{
+
+        }
     }
+
+    private fun createPage(){
+        binding.titleDet.text=bundle!!.getString("title")
+        binding.descriptionDet.text=bundle!!.getString("description")
+
+        val img = binding.imageView3
+        val url=bundle!!.getString("photoLink")
+
+        Glide.with(img)
+            .load(url)
+            .placeholder(R.drawable.image)
+            .error(R.drawable.image)
+            .fallback(R.drawable.image)
+            .into(img)
+
+    }
+
 }
--- app/src/main/java/com/example/finalandroid/movies/Home.kt
+++ app/src/main/java/com/example/finalandroid/movies/Home.kt
@@ -108,4 +108,6 @@ class Home : Fragment() {
         fun newInstance() = Home()
 
     }
+
+
 }
