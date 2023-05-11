c962bc52c0ea5a760945b41067b6fa0bf7e837b0
Togzhan Kali <t_kali@kbtu.kz>
2023-05-11 12:42:34 +0600

    dfxfhkjl

--- app/src/main/java/com/example/finalandroid/FirstWelcome.kt
+++ app/src/main/java/com/example/finalandroid/FirstWelcome.kt
@@ -1,5 +1,6 @@
 package com.example.finalandroid
 
+import android.content.Intent
 import android.os.Bundle
 import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
@@ -7,6 +8,8 @@ import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
+import com.example.finalandroid.activities.Login_page
+import com.example.finalandroid.activities.Sign_up
 import com.example.finalandroid.databinding.ActivityFirstWelcomeBinding
 
 class FirstWelcome : AppCompatActivity() {
@@ -16,6 +19,23 @@ class FirstWelcome : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding= ActivityFirstWelcomeBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+        binding.singin.setOnClickListener{
+            val intent= Intent(this, Login_page::class.java)
+            startActivity(intent)
+        }
+
+        binding.singup.setOnClickListener{
+            val intent= Intent(this, Sign_up::class.java)
+            startActivity(intent)
+        }
+
+
+
+
 
 
     }
